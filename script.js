b60625036e1b181f342a27c60954b7405e441013
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-08-13 18:45:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-//your code here!
+const list = document.getElementById("infi-list");
+let itemCount = 1;
 
+function addListItem() {
+  const li = document.createElement("li");
+  li.textContent = `Item ${itemCount}`;
+  list.appendChild(li);
+  itemCount++;
+}
+
+// Add first 10
+for (let i = 0; i < 10; i++) {
+  addListItem();
+}
+
+// Listen for scrolling *inside the container*
+list.addEventListener("scroll", () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+	 
+    for (let i = 0; i < 2; i++) {
+      addListItem();
+    }
+  }
+});
